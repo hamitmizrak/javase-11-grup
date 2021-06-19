4b343f9a1c68646433be7d8f598d03615089dc2d
HamitMizrak <hamitmizrak@gmail.com>
2021-06-19 12:21:08 +0300

    Escape character

---
+++ src/com/ecodation/dersler/Ders_009_escape.java
@@ -0,0 +1,16 @@
+package com.ecodation.dersler;
+
+public class Ders_009_escape {
+
+	public static void main(String[] args) {
+
+		// escape character: çıkış karakteri diyoruz yani javadaki özel karakterleri göstermek için kullanıyoruz.
+		// \n=alt satır
+		// \r=en başa
+		// \t=tab
+		// \" =tırnak işareti çıkarmak için
+		System.out.println("\"");
+		// \'
+
+	}
+}
