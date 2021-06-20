03ab1e3e6ad35c93ee33c58eec9f266b8fb860f5
HamitMizrak <hamitmizrak@gmail.com>
2021-06-20 10:53:34 +0300

    Ternary

---
+++ src/com/ecodation/dersler/Ders_022_Ternary.java
@@ -0,0 +1,10 @@
+package com.ecodation.dersler;
+
+public class Ders_022_Ternary {
+	public static void main(String[] args) {
+
+		int sayi = 2;
+		String sonuc = (sayi == 2) ? "sayınız 2'dir" : "yanlış girdiniz"; // ternary
+		System.out.println(sonuc);
+	}
+}
