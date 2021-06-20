1cfbc3e8a6838622247dde2a804851cdb7257310
HamitMizrak <hamitmizrak@gmail.com>
2021-06-20 10:53:49 +0300

    Conditional (Karar mekanizması)

---
+++ src/com/ecodation/dersler/Ders_021_KararMekanizmasi.java
@@ -0,0 +1,34 @@
+package com.ecodation.dersler;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Ders_021_KararMekanizmasi {
+	public static void main(String[] args) {
+
+		int rastgele, kullaniciSayi;
+		Scanner klavye = new Scanner(System.in);
+
+		Random random = new Random();
+		rastgele = random.nextInt(15) + 1;
+		System.out.println(rastgele);
+
+		while (true) {
+			System.out.println("1 ile 15 arasında bir sayı giriniz.");
+			kullaniciSayi = klavye.nextInt();
+
+			if (rastgele == kullaniciSayi) {
+				System.out.println("Tebrik ederim sayıyı buldunuz");
+			} else if (kullaniciSayi > rastgele) {
+				System.out.println(
+						"Büyük sayı girdiniz: " + " tahmin edilen sayı arasındaki fark: " + (kullaniciSayi - rastgele));
+			} else if (kullaniciSayi < rastgele) {
+				System.out.println(
+						"Küçük sayı girdiniz" + " tahmin edilen sayı arasındaki fark: " + (rastgele - kullaniciSayi));
+			} else {
+				System.out.println("Lütfen belirtilen aralıkta sayı giriniz");
+			}
+		}
+
+	}
+}
